4ee00437ff386cf091d51fec4f1b07c7ab87b011
mahendra <mahendrasuthar02@gmail.com>
2018-07-20 21:53:28 +0530

    bg added

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express"),
     app     = express();
-    
+
+
+app.use(express.static(__dirname + '/public'));
 app.set("view engine","ejs");
 
 
@@ -14,6 +16,6 @@ app.get("/find", (req,res)=>{
 });
 
 
-app.listen(process.env.PORT, process.env.IP, function() {
+app.listen(3000, function() {
     console.log("server has started");
 });
