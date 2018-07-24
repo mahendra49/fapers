b8d4941468fdf02846411c9db87c7f97267bf5c8
mahendra <mahendrasuthar02@gmail.com>
2018-07-24 18:55:43 +0530

    testing

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 var express     = require("express"),
     app         = express(),
     bodyparser  = require("body-parser"),
-    mongoose    = require("mongoose");
+    mongoose    = require("mongoose"),
+    User = require("./models/users"),
+    passport =require("passport"),
+    Localstartegy = require("passport-local"),
+    passportlocalmongoose = require("passport-local-mongoose"),
+    expressSanitizer = require("express-sanitizer"); 
+
+
+
+
+passport.use(new Localstartegy(User.authenticate()));
+
+app.use(require("express-session")({
+    secret: "This is my first autun demo",
+    resave: false,
+    saveUninitialized: false
+}));
 
 
 mongoose.connect("mongodb://localhost/faper");
@@ -12,17 +28,41 @@ app.use(express.static(__dirname + '/public'));
 app.set("view engine","ejs");
 
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 // route for home page
 app.get("/",function(req,res){
     res.render("home");
 });
 
-//register logic goes here
-app.post("/register" ,function(req,res){
-    console.log(req.body.user);
 
-    res.render("profile");
+//middleware here before actually logging in
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res) {
+        console.log("success log in ");
+        res.render("profile");
+});
 
+app.post("/register", function(req, res) {
+
+    var userdata = req.body.user;
+    
+    User.register(new User({ username: userdata.username }), userdata.password, function(err, user) {
+        if (err) {
+            console.log("error");
+            res.render("/register");
+        }
+        passport.authenticate("local")(req, res, function() {
+            res.redirect("/userprofile");
+        });
+    });
 });
 
 
@@ -30,6 +70,10 @@ app.post("/register" ,function(req,res){
 //login route
 app.get("/login",function(req,res){
     res.render("login");
+});
+
+app.get("/userprofile",function(req,res){
+    res.render("profile");
 });
 
 //register 
@@ -46,24 +90,26 @@ app.get("/findfaper",function(req,res){
     res.render("fapers");
 });
 
-// login logic goes here and auth logic goes here
-app.post("/login",function(req,res){
-    
-    // check for authentication here
-    console.log(req.body.login);
 
-    // database query goes here
-
-
-    // query result goes to profile template
-    var profile = req.body.login;
-    res.render("profile",{profile:profile});
-});
 
 //fapers
 app.get("/fapers",function(req,res){
     res.render("fapers");
 });
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 
 app.listen(3000, function() {
     console.log("server has started");
